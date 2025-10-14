7596ced794c829452493ea1953e7544c085c20d9
freeecondata <freeecondata@gmail.com>
2025-10-14 10:02:26 -0500

    update repo

---
+++ week-12/data/week-12.r
@@ -0,0 +1,35 @@
+## --- Generación de la base simulada ---
+require(dplyr)
+
+# Lista de jugadores
+jugadores <- c("Vargas", "Cuesta", "Mina", "Machado", "Muñoz",
+               "Uribe", "Lerma", "James", "Luis Díaz", "Arias", "Borré")
+
+set.seed(123)
+
+# Generar pases de forma aleatoria
+sel_colombia <- data.frame(
+  from = sample(jugadores, 35, replace = TRUE),
+  to   = sample(jugadores, 35, replace = TRUE)
+) %>%
+  filter(from != to)
+
+# Aumentar la centralidad de James (más pases salientes y algunos entrantes)
+james_extra <- data.frame(
+  from = rep("James", 15),
+  to   = sample(jugadores[jugadores != "James"], 15, replace = TRUE)
+)
+
+# Algunos pases dirigidos hacia James (para simular recepciones)
+hacia_james <- data.frame(
+  from = sample(jugadores[jugadores != "James"], 5, replace = TRUE),
+  to   = rep("James", 5)
+)
+
+# Unir todo y agregar pesos
+sel_colombia <- bind_rows(sel_colombia, james_extra, hacia_james) %>% count(from, to, name = "peso") %>%
+                pivot_wider(names_from = to, values_from = peso, values_fill = 0) %>%
+                column_to_rownames("from") %>%
+                as.matrix()
+
+rm(hacia_james,james_extra,jugadores)
